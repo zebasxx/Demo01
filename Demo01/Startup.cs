4f420e8ea098178cd8942307439ea0b95fe9a271
zebasxx <zebasxx@hotmail.com>
2018-07-31 14:10:23 -0300

    Change Startup.cs

--- Demo01/Startup.cs
+++ Demo01/Startup.cs
@@ -27,7 +27,7 @@ namespace Demo01
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Demo!!!!!");
             });
         }
     }
